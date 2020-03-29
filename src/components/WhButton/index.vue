88c90a234f0000cf91b3da6d1d86021ba52229c0
miracletjf <1398988546@qq.com>
2020-03-29 23:46:53 +0800

    chore: 配置 css 全局变量，配置 全局 iconfont 地址

---
+++ src/components/WhButton/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="wh-button"></button>
+</template>
+
+<script>
+export default {
+  name: 'WhButton',
+  props: {
+  },
+};
+</script>
+
+<style lang="scss">
+  .wh-button {
+
+  }
+</style>
